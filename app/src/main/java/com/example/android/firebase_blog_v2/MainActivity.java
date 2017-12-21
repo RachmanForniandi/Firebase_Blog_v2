275b8987fcbe93ff76130261e67fcebe2cba6c46
RachmanForniandi <rachmanforniandi@gmail.com>
2017-12-21 18:10:30 +0700

    add main menu

---
+++ app/src/main/java/com/example/android/firebase_blog_v2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.firebase_blog_v2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
