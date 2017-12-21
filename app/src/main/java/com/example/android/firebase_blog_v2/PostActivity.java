4216354b90e1595f6df7e8b5108fb5e29eb61e60
RachmanForniandi <rachmanforniandi@gmail.com>
2017-12-21 22:42:14 +0700

    select data image by PostActivity

--- app/src/main/java/com/example/android/firebase_blog_v2/PostActivity.java
+++ app/src/main/java/com/example/android/firebase_blog_v2/PostActivity.java
@@ -1,13 +1,44 @@
 package com.example.android.firebase_blog_v2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+    private ImageButton mSelectImage;
+    private static final int GALLERY_REQUEST = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mSelectImage = (ImageButton)findViewById(R.id.imgButton);
+
+            mSelectImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    galleryIntent.setType("image/*");
+                    startActivityForResult(galleryIntent, GALLERY_REQUEST);
+                }
+            });
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+
+            Uri imageUri = data.getData();
+            mSelectImage.setImageURI(imageUri);
+
     }
+  }
 }
